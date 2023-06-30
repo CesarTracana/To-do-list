8f044c8912c21d5909ee78f6d6181c6862e0c491
CesarTracana <cedtg12@gmail.com>
2023-06-30 17:25:01 -0400

    listar

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+nombre="Cesar"
+lista_nombres=['Brhadaran', 'Yako', 'Panisha', 'Dviveka', 'Chudamani', 'Erreh']
 #ruta
 @app.route('/')
 #vista
-def index():
-    return render_template('base.html')
+def home():
+    return render_template('home.html', nombre=nombre, lista_nombres=lista_nombres)
+
+""" @app.route('/about')
+def about(nombre):
+    return f' !hols, yo soy {nombre} y estudio ingenieria en computacion
+"""
