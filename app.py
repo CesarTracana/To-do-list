f608a70350fc750629ba68c60e5a7c10d5215c21
CesarTracana <cedtg12@gmail.com>
2023-07-07 17:36:11 -0400

    MUCHOTEXTO

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app=Flask(__name__)
 
 nombre="Cesar"
-lista_nombres=['Brhadaran', 'Yako', 'Panisha', 'Dviveka', 'Chudamani', 'Erreh']
+lista_tareas=[]
 #ruta
 @app.route('/')
 #vista
 def home():
-    return render_template('home.html', nombre=nombre, lista_nombres=lista_nombres)
+    return render_template('home.html', nombre=nombre, lista_tareas=lista_tareas)
+
+#ruta
+@app.route('/agregar', methods=['GET', 'POST'])
+#vista
+def agregar():
+        if request.method=='POST':
+              nueva_tarea=request.form.get('tarea')
+              lista_tareas.append(nueva_tarea)
+        return redirect('/')
+
 
 """ @app.route('/about')
 def about(nombre):
